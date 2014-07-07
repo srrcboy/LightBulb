7e59a9504a208be08d18196dbc285ec10ccda7c8
秉亮 <bl_indie@qq.com>
2014-07-07 23:38:53 +0800

    Opencv 2.4.9 set

--- LightBulb/main.cpp
+++ LightBulb/main.cpp
@@ -1,10 +1,18 @@
 #include "lightbulb.h"
 #include <QtWidgets/QApplication>
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	LightBulb w;
+	
+	Mat img = imread("pablo-picasso.jpg");
+	namedWindow("image",CV_WINDOW_AUTOSIZE);  
+	imshow("image",img);  
+	//cv::waitKey(); 
 	w.show();
 	return a.exec();
 }
